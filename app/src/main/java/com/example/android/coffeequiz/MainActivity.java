72be072ee8a20b4e33a1513a0ef14e25c88294d1
Krzysztof Kin <sirex0@gmail.com>
2018-01-25 20:49:12 +0100

    Feedback on share, name in popup, code cleaning

--- app/src/main/java/com/example/android/coffeequiz/MainActivity.java
+++ app/src/main/java/com/example/android/coffeequiz/MainActivity.java
@@ -2,16 +2,19 @@ package com.example.android.coffeequiz;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     boolean[] hasGoodAnswer = {false, false, false, false, false, false};
+    String name = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +28,42 @@ public class MainActivity extends AppCompatActivity {
      * @param view view
      */
     public void onCheckPress(View view) {
-        //odpowiedziałeś poprawnie na x pytań
-        //źle odpowiedziałeś na pytania nr
+
+        //check all questions and ignore feedback
+        checkAll();
+        String message = getString(R.string.hello, name) + "\n";
+        message = message + getString(R.string.good_answers, goodAnswers() + "");
+        showMessage(message);
+    }
+
+    /**
+     * Check all questions
+     *
+     * @return feedback to share
+     */
+    private String checkAll() {
         String feedback;
-        String feedback1, feedback2, feedback3, feedback4, feedback5, feedback6;
+        String feedback1;
+        String feedback2;
+        String feedback3;
+        String feedback4;
+        String feedback5;
 
         feedback1 = checkFirstAnswer();
         feedback2 = checkSecondAnswer();
         feedback3 = checkThridAnswer();
         feedback4 = checkFourthAnswer();
         feedback5 = checkFifthAnswer();
-        feedback6 = checkSixthAnswer();
+        name = checkSixthAnswer();
 
-        feedback = feedback1;
+        feedback = getString(R.string.hello, name) + "\n";
+        feedback = feedback + feedback1;
         feedback = feedback + feedback2;
         feedback = feedback + feedback3;
-//        feedback = feedback + feedback4;
-//        feedback = feedback + feedback5;
-//        feedback = feedback + feedback6;
+        feedback = feedback + feedback4;
+        feedback = feedback + feedback5;
 
-        showMessage(goodAnswers() + "\n" + feedback);
+        return feedback;
     }
 
     /**
@@ -53,7 +72,8 @@ public class MainActivity extends AppCompatActivity {
      * @param view view
      */
     public void onSharePress(View view) {
-        shareMessage(getString(R.string.question_placeholder));
+        String feedback = checkAll();
+        shareMessage(feedback);
     }
 
     /**
@@ -75,9 +95,10 @@ public class MainActivity extends AppCompatActivity {
      */
     public void shareMessage(String message) {
         Intent sendIntent = new Intent(Intent.ACTION_SEND);
-        //sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.setData(Uri.parse("mailto:"));
         sendIntent.setType("text/plain");
         sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name));
 
         // Verify that the intent will resolve to an activity
         if (sendIntent.resolveActivity(getPackageManager()) != null) {
@@ -85,6 +106,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Count good answers
+     *
+     * @return good answers
+     */
     private int goodAnswers() {
         int counter = 0;
         if (hasGoodAnswer[0]) counter++;
@@ -92,7 +118,6 @@ public class MainActivity extends AppCompatActivity {
         if (hasGoodAnswer[2]) counter++;
         if (hasGoodAnswer[3]) counter++;
         if (hasGoodAnswer[4]) counter++;
-        if (hasGoodAnswer[5]) counter++;
         return counter;
     }
 
@@ -102,13 +127,14 @@ public class MainActivity extends AppCompatActivity {
      * @return descriptive rating
      */
     private String checkFirstAnswer() {
-        hasGoodAnswer[0] = false;
         RadioButton radio = findViewById(R.id.q1a1);
         if (radio.isChecked()) {
             hasGoodAnswer[0] = true;
-            return "";
+            return (getString(R.string.question_1_nr) + getString(R.string.good_answer) + "\n");
+        } else {
+            hasGoodAnswer[0] = false;
+            return (getString(R.string.question_1_answer) + "\n");
         }
-        return (getString(R.string.question_1_answer) + "\n");
     }
 
     /**
@@ -117,16 +143,16 @@ public class MainActivity extends AppCompatActivity {
      * @return descriptive rating
      */
     private String checkSecondAnswer() {
-        hasGoodAnswer[1] = false;
         CheckBox checkBox1 = findViewById(R.id.q2a1);
         CheckBox checkBox2 = findViewById(R.id.q2a2);
         CheckBox checkBox3 = findViewById(R.id.q2a3);
         if (checkBox1.isChecked() & checkBox2.isChecked() & !checkBox3.isChecked()) {
             hasGoodAnswer[1] = true;
-            return "";
+            return (getString(R.string.question_2_nr) + getString(R.string.good_answer) + "\n");
+        } else {
+            hasGoodAnswer[1] = false;
+            return (getString(R.string.question_2_answer) + "\n");
         }
-
-        return (getString(R.string.question_2_answer)+"\n");
     }
 
     /**
@@ -135,13 +161,14 @@ public class MainActivity extends AppCompatActivity {
      * @return descriptive rating
      */
     private String checkThridAnswer() {
-        hasGoodAnswer[2] = false;
         RadioButton radio = findViewById(R.id.q3a1);
         if (radio.isChecked()) {
             hasGoodAnswer[2] = true;
-            return "";
+            return (getString(R.string.question_3_nr) + getString(R.string.good_answer) + "\n");
+        } else {
+            hasGoodAnswer[2] = false;
+            return (getString(R.string.question_3_answer) + "\n");
         }
-        return (getString(R.string.question_3_answer) + "\n");
     }
 
     /**
@@ -150,8 +177,14 @@ public class MainActivity extends AppCompatActivity {
      * @return descriptive rating
      */
     private String checkFourthAnswer() {
-        hasGoodAnswer[3] = false;
-        return getString(R.string.question_placeholder);
+        RadioButton radio = findViewById(R.id.q4a1);
+        if (radio.isChecked()) {
+            hasGoodAnswer[3] = true;
+            return (getString(R.string.question_4_nr) + getString(R.string.good_answer) + "\n");
+        } else {
+            hasGoodAnswer[3] = false;
+            return (getString(R.string.question_4_answer) + "\n");
+        }
     }
 
     /**
@@ -160,8 +193,15 @@ public class MainActivity extends AppCompatActivity {
      * @return descriptive rating
      */
     private String checkFifthAnswer() {
-        hasGoodAnswer[4] = false;
-        return getString(R.string.question_placeholder);
+        EditText editText = findViewById(R.id.year);
+        String yearString = editText.getText().toString();
+        if (yearString.equals("1884")) {
+            hasGoodAnswer[4] = true;
+            return (getString(R.string.question_5_nr) + getString(R.string.good_answer) + "\n");
+        } else {
+            hasGoodAnswer[4] = false;
+            return (getString(R.string.question_5_answer) + "\n");
+        }
     }
 
     /**
@@ -170,7 +210,7 @@ public class MainActivity extends AppCompatActivity {
      * @return descriptive rating
      */
     private String checkSixthAnswer() {
-        hasGoodAnswer[5] = false;
-        return getString(R.string.question_placeholder);
+        EditText editText = findViewById(R.id.name);
+        return editText.getText().toString();
     }
 }
